343b1b686e5094f39e94f2514fd846141efb83f7
John Howard <howardjohn@google.com>
2019-05-29 17:06:52 -0700

    Write to file

--- main.go
+++ main.go
@@ -3,30 +3,27 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"github.com/gogo/protobuf/types"
+	"io/ioutil"
+	"log"
+	"os"
+	"sigs.k8s.io/yaml"
 	"time"
 
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/gogo/protobuf/proto"
 
+	pilotv2 "istio.io/istio/pilot/pkg/proxy/envoy/v2"
 	"istio.io/istio/pkg/adsc"
 )
 
-//const (
-//	typePrefix = "type.googleapis.com/envoy.api.v2."
-//
-//	// Constants used for XDS
-//
-//	// ClusterType is used for cluster discovery. Typically first request received
-//	clusterType = typePrefix + "Cluster"
-//	// EndpointType is used for EDS and ADS endpoint discovery. Typically second request.
-//	endpointType = typePrefix + "ClusterLoadAssignment"
-//	// ListenerType is sent after clusters and endpoints.
-//	listenerType = typePrefix + "Listener"
-//	// RouteType is sent after listeners.
-//	routeType = typePrefix + "RouteConfiguration"
-//)
+var (
+	OUTDIR = os.Getenv("OUTDIR")
+)
 
 func main() {
+	log.SetFlags(0)
 	grpc := fmt.Sprintf("localhost:15010")
 	adsc, err := adsc.Dial(grpc, "", &adsc.Config{
 		IP:        "10.11.0.1",
@@ -37,26 +34,50 @@ func main() {
 	}
 	fmt.Println("Waiting for updates")
 	adsc.Watch()
-	for {
-		select {
-		case r := <-adsc.Updates:
-			fmt.Printf("Got small update: %v\n", r)
-			for _, c := range adsc.Clusters {
-				fmt.Println(MarshallJson(c))
-			}
-		case <-time.After(time.Second * 10):
-			fmt.Println("Done")
-			return
-		}
+	_, err = adsc.Wait("eds", 10*time.Second)
+	clusters := []*v2.Cluster{}
+	for _, c := range adsc.Clusters {
+		clusters = append(clusters, c)
+	}
+	for _, c := range adsc.EDSClusters {
+		clusters = append(clusters, c)
 	}
 
+	write(clusterResponse(clusters), "cds.yaml")
 }
 
-func MarshallJson(w proto.Message) string {
+func write(r *v2.DiscoveryResponse, out string) {
+	if OUTDIR == "" {
+		fmt.Println(string(MarshallYaml(r)))
+	} else {
+		if err := ioutil.WriteFile(OUTDIR + "/" + out, MarshallYaml(r), 0777); err != nil {
+			panic(err)
+		}
+	}
+}
+func clusterResponse(response []*v2.Cluster) *v2.DiscoveryResponse {
+	out := &v2.DiscoveryResponse{
+		TypeUrl:     pilotv2.ClusterType,
+		VersionInfo: "0",
+	}
+
+	for _, c := range response {
+		cc, _ := types.MarshalAny(c)
+		out.Resources = append(out.Resources, *cc)
+	}
+
+	return out
+}
+func MarshallJson(w proto.Message) []byte {
 	buffer := &bytes.Buffer{}
 	err := (&jsonpb.Marshaler{}).Marshal(buffer, w)
 	if err != nil {
-		return ""
+		return []byte{}
 	}
-	return buffer.String()
+	return buffer.Bytes()
+}
+
+func MarshallYaml(w proto.Message) []byte {
+	b, _ := yaml.JSONToYAML([]byte(MarshallJson(w)))
+	return b
 }
